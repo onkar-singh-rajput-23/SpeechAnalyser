e203201d1fd02da6312c4819d934e387affbc451
Onkar Singh Rajput <onkar.rajput@travenues.com>
2025-11-21 01:31:10 +0530

    Initial Commit

---
+++ NoteTaker/NoteTakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTakerApp.swift
+//  NoteTaker
+//
+//  Created by onkar.rajput on 21/11/25.
+//
+
+import SwiftUI
+
+@main
+struct NoteTakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NoteTakerTests/NoteTakerTests.swift
@@ -0,0 +1,16 @@
+//
+//  NoteTakerTests.swift
+//  NoteTakerTests
+//
+//  Created by onkar.rajput on 21/11/25.
+//
+
+import Testing
+
+struct NoteTakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
